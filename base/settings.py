7632af8ab9bcec9e3f6b14b7ca87266cc6ecd9ff
rajat-gith <rajatnayak1582002@gmail.com>
2022-11-06 13:10:05 +0530

    Commit

--- base/settings.py
+++ base/settings.py
@@ -103,7 +103,7 @@ ROOT_URLCONF = "base.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR,"templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -161,9 +161,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "staticfiles")
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from base import views 
 
 urlpatterns = [
+    path("", views.homepage), 
     path("admin/", admin.site.urls),
     path("api/ngos/",include('src.urls.ngo_urls')),
     path('api/users/',include('src.urls.donor_urls')),
