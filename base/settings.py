46897f45b6a60b521d812ed66ec6b5766d452efd
rajat-gith <rajatnayak1582002@gmail.com>
2022-11-06 12:36:51 +0530

    Commit

--- base/settings.py
+++ base/settings.py
@@ -162,7 +162,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-
     os.path.join(BASE_DIR, "static")
 ]
 # Default primary key field type
