f6fc0a9a505a73d8a8b8513cac4ae4e7da4daba1
rajat-gith <rajatnayak1582002@gmail.com>
2022-11-11 18:01:11 +0530

    Add NGO Api Added

--- src/urls/ngo_urls.py
+++ src/urls/ngo_urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from src.views import ngo_views as views
 
 urlpatterns=[
+    path('add/',views.addNgo,name="ngoadd"),
     path('',views.getNgos,name="ngos"),
     path('<str:pk>/',views.getNgo,name="ngo"),
 ]
--- src/views/ngo_views.py
+++ src/views/ngo_views.py
@@ -1,8 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from src import serializer
 from src.models import Ngo
 from src.serializer import NgoSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
 def getNgos(request):
@@ -15,4 +16,20 @@ def getNgo(request,pk):
     ngo=Ngo.objects.get(_id=pk)
     serializer=NgoSerializer(ngo,many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addNgo(request):
+    data=request.data
+    ngoObject= Ngo.objects.create(
+        name=data['name'],
+        location=data['location'],
+        desc=data['desc'],
+        ngo_owner=data['ngo_owner'],
+        tagline=data['tagline']
+    )
+
+    serializer=NgoSerializer(ngoObject,many=False)
+    return Response(serializer.data)
+
 
