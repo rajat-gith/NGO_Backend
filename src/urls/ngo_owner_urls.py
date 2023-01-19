468935fc8d5e0ca76742ff3ecfe4e002e5892e27
rajat-gith <rajatnayak1582002@gmail.com>
2023-01-19 23:09:03 +0530

    Added Urls

--- src/urls/ngo_owner_urls.py
+++ src/urls/ngo_owner_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from src.views import ngoOwner_views as views
-
+from src.views import donor_views as view
 
 urlpatterns=[
     path('', views.getOwners, name='token_obtain_pair'),
     path('login/', views.MyTokenObtainPairViewOwner.as_view(), name='token_obtain_pair'),
     path('register/',views.registerOwner,name='register'),
+    path('profile/',view.getUserProfile,name="user-profile"),
 ]
